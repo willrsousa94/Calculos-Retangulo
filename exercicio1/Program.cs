ca141da12487d9f5225e5c460b6644b0808eac6c
William Rodrigues de Sousa <rm78369@fiap.com.br>
2017-06-26 00:22:58 -0300

    commit VS 1.0

---
+++ exercicio1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio1 {
+    class Program {
+        static void Main(string[] args) {
+            retangulo valor;
+
+            valor = new retangulo();
+
+            valor.l = double.Parse(Console.ReadLine());
+            valor.h = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(valor);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ exercicio1/retangulo.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exercicio1 {
+    class retangulo {
+        public double h;
+        public double l;
+
+        public double area() {
+            return h * l;
+        }
+
+        public double perimetro() {
+            return 2 * (h + l);
+        }
+
+        public double diagonal() {
+            return Math.Sqrt((h * h) + (l * l));
+        }
+
+        public override string ToString() {
+            return "AREA = "
+                + area()
+                + Environment.NewLine
+                + "PERIMETRO = "
+                + perimetro()
+                + Environment.NewLine
+                + "DIAGONAL = "
+                + diagonal();
+        }
+    }
+}
